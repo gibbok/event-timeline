1234597afe5c2bac6d04f7a790f1e98699bb2f2f
GibboK <gibbok.coding@gmail.com>
2023-03-23 19:46:51 +0100

    init app axios

--- event-timeline/pages/_app.tsx
+++ event-timeline/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { initApp } from "@/configs/init";
+import type { AppProps } from "next/app";
+
+initApp();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- event-timeline/pages/index.tsx
+++ event-timeline/pages/index.tsx
@@ -1,12 +1,4 @@
-import { initApp } from '@/configs/init';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 import Head from "next/head";
-
-initApp()
 
 export default function Home() {
   return (
