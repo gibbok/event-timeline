81e34a2aaa858a746c9fc1c1afdbfa35d18e2e6e
GibboK <gibbok.coding@gmail.com>
2023-03-24 15:52:59 +0100

    format

--- event-timeline/utils/format.ts
+++ event-timeline/utils/format.ts
@@ -1,9 +1,1 @@
-export const formatDateTime = (dateTime: string) =>
-    new Date(dateTime).toLocaleDateString('en-US', {
-        weekday: 'short',
-        year: 'numeric',
-        day: '2-digit',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric'
-    })
+export const formatDateTime = (dateTime: string) => new Date(dateTime).toLocaleString()
