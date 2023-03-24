94b97ddb63d7183b124098428b6f4d7580c8448c
GibboK <gibbok.coding@gmail.com>
2023-03-24 16:40:46 +0100

    render color text

--- event-timeline/components/apps/EventTimeline/EventInfo.tsx
+++ event-timeline/components/apps/EventTimeline/EventInfo.tsx
@@ -1,3 +1,4 @@
+import { JobEventTypeAPI } from "@/api/types";
 import { EventUI } from "@/pages/transform";
 import { formatDateTime } from "@/utils/format";
 import { Box, Card, CardContent, CardHeader, Typography } from "@mui/material";
@@ -6,6 +7,29 @@ import { CausesInfo } from "./CausesInfo";
 import { DeviceInfo } from "./DeviceInfo";
 
 type EventInfoProps = EventUI;
+
+const makeColor = (eventType: JobEventTypeAPI) => {
+  switch (eventType) {
+    case JobEventTypeAPI.Scheduled:
+    case JobEventTypeAPI.Started:
+    case JobEventTypeAPI.InProgress:
+    case JobEventTypeAPI.Complete:
+      return "success.main";
+    case JobEventTypeAPI.OnBreak:
+      return "red";
+    case JobEventTypeAPI.ClockedOut:
+      return "green";
+    case JobEventTypeAPI.DowntimeReportSubmitted:
+    case JobEventTypeAPI.OverrunReportSubmitted:
+    case JobEventTypeAPI.HeadcountChanged:
+    case JobEventTypeAPI.SubmitDowntimeReportCauses:
+    case JobEventTypeAPI.JobSignedOff:
+      return "text.primary";
+    case JobEventTypeAPI.Canceled:
+    case JobEventTypeAPI.Downtime:
+      return "error.main";
+  }
+};
 
 export const EventInfo = ({
   id,
@@ -21,8 +45,8 @@ export const EventInfo = ({
       <Box display="flex" justifyContent="space-between">
         <Box>
           <Typography
-            style={{ textTransform: "uppercase" }}
-            color="text.primary"
+            style={{ textTransform: "uppercase", fontWeight: "bold" }}
+            color={makeColor(type)}
             gutterBottom
           >
             {type}
