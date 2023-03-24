d37d3b11952cfb3e80c981cedb6d7a386e8992cb
GibboK <gibbok.coding@gmail.com>
2023-03-24 23:48:08 +0100

    save

--- event-timeline/components/apps/EventTimeline/EventTimeline.test.tsx
+++ event-timeline/components/apps/EventTimeline/EventTimeline.test.tsx
@@ -5,39 +5,6 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { EventTimeline } from "./EventTimeline";
 
-const TEST_DATA = [
-  {
-    eventId: "be317a24-09b8-4b5b-997b-a8542a1738a2",
-    cardId: "ce6bff8d-eb09-4208-9405-c2594f74ccb0",
-    type: "downtime_report_submitted",
-    userName: "Thad Cormier",
-    occurrence: "2022-11-23 17:40:49.193277+00",
-    device: {
-      deviceId:
-        "d85922780c45ad4eadc0919e75f964085db7649b2dcb7fd78b76f8d65ce09cd3",
-      zoneName: "Bench - Panels",
-      factoryName: "Urban Splash House Factory",
-      partnerName: "Urban Splash",
-      stationName: "Bay 1",
-    },
-  },
-  {
-    eventId: "e6c2c29d-156d-49ca-9c55-59c6ff464892",
-    cardId: "ce6bff8d-eb09-4208-9405-c2594f74ccb0",
-    type: "in_progress",
-    userName: "Thad Cormier",
-    occurrence: "2022-11-23 17:40:46.800652+00",
-    device: {
-      deviceId:
-        "d85922780c45ad4eadc0919e75f964085db7649b2dcb7fd78b76f8d65ce09cd3",
-      zoneName: "Bench - Panels",
-      factoryName: "Urban Splash House Factory",
-      partnerName: "Urban Splash",
-      stationName: "Bay 1",
-    },
-  },
-] as EventsUI;
-
 describe("EventTimeline", () => {
   it("should render progress bar when loading data", () => {
     render(<EventTimeline status="loading" />);
@@ -65,7 +32,7 @@ describe("EventTimeline", () => {
     render(
       <EventTimeline
         status="loaded"
-        data={TEST_DATA}
+        data={eventDataUI.slice(0, 2)}
         page={1}
         countPages={1}
         onChangePage={onChangePage}
@@ -92,7 +59,7 @@ describe("EventTimeline", () => {
     render(
       <EventTimeline
         status="loaded"
-        data={TEST_DATA.slice(0, 1)}
+        data={eventDataUI.slice(0, 1)}
         page={1}
         countPages={1}
         onChangePage={onChangePage}
